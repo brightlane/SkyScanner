de166b1aa75cb90c41cd17225221fc9c75a0e6d3
brightlane <benitopalmarini@aol.com>
2026-04-18 22:42:33 -0400

    Update affiliate link generation with dynamic query

--- affiliate-links.js
+++ affiliate-links.js
@@ -4,6 +4,11 @@
     const affiliateBaseURL = "https://convert.ctypy.com/aff_c?offer_id=29465&aff_id=";
     const affiliateID = "21885";  // Replace this with your actual affiliate ID
 
+    // Function to generate dynamic query parameters based on target audience or location
+    function generateAffiliateURL(query) {
+        return `${affiliateBaseURL}${affiliateID}&query=${encodeURIComponent(query)}`;
+    }
+
     // List of IDs for elements on your pages where affiliate links will go
     const affiliateElements = [
         'affiliate-link-1', // ID of the first link element
@@ -14,10 +19,15 @@
 
     // Function to update the href attributes of elements with the affiliate link
     function updateAffiliateLinks() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const query = urlParams.get('q');  // Get the query parameter from the URL (e.g. MetLife, Bali, etc.)
+
+        const affiliateURL = query ? generateAffiliateURL(query) : generateAffiliateURL('default');
+
         affiliateElements.forEach(function(elementId) {
             const element = document.getElementById(elementId);
             if (element) {
-                element.setAttribute('href', affiliateBaseURL + affiliateID);
+                element.setAttribute('href', affiliateURL);
             }
         });
     }
